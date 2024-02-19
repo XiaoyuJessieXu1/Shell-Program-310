6d665eb893a600e6cc7d6b7b1089d9f77c5e83e9
Jessie <112979341+XiaoyuJessieXu1@users.noreply.github.com>
2024-02-19 17:37:00 -0500

    Delete src directory

--- src/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef FRAME_SIZE
-#define FRAME_SIZE 0
-#endif
-
-#ifndef VAR_MEM_SIZE
-#define VAR_MEM_SIZE 0
-#endif
--- src/shellmemory.h
+++
@@ -1,4 +0,0 @@
-void mem_init();
-int mem_set_value(char *var, char *value);
-char *mem_get_value(char *var);
-void printsm();
